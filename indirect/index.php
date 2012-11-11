e966cfd5ca8b11928d6d42391e3043979826794d
pavels <pavels@192.168.1.5>
2012-11-11 12:48:53 -0800

    I made a mistake of using absolute URLs in a form of '/?'. These will only work in blogs with DirectryIndex setup in the .htaccess, and many blogs don't have it. Now we properly use relative URLs.

--- indirect/index.php
+++ indirect/index.php
@@ -23,14 +23,14 @@
 
 */
 
-// go up to the root folder and load WordPress
+// go to the root folder and load Wordpress
 require_once "../../../../../wp-config.php";
 
 // check if plugin is installed and render the dynamic content
-if (!function_exists('ity_ef_render_indirect')){
-	echo 'Failed to find ity_ef_render_indirect() function. Is Khan Exercises Plugin active?';
+if (!function_exists('ity_ef_controller')){
+	echo 'Failed to find ity_ef_controller() function. Is Khan Exercises Plugin active?';
 } else {
-	ity_ef_render_indirect();
+	ity_ef_controller();
 }
 
 ?>
